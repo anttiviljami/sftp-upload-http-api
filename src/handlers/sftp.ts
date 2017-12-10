e5b556c8c8fa1d0096e66732a5045c0bba6d348e
Viljami Kuosmanen <anttiviljami@futurice.com>
2017-12-10 12:43:42 +0200

    Safe upload path handling, better response

--- src/handlers/sftp.ts
+++ src/handlers/sftp.ts
@@ -2,24 +2,30 @@ import * as BPromise from 'bluebird';
 import * as Hapi from 'hapi';
 import * as Boom from 'boom';
 import * as logger from 'winston';
+import * as path from 'path';
 
-import { sftpClient } from '../util/sftp';
+import { sftpClient, SFTPWriteStream } from '../util/sftp';
+import { SFTPStream } from 'ssh2-streams';
 export async function uploadHandler(req: Hapi.Request, reply: Hapi.ReplyNoContinue) {
   const { file } = req.payload;
   if (!file) {
     throw Boom.badData('Failed to read file');
   }
   try {
-    const { filename } = file.hapi;
+    const { filename, headers, bytes } = file.hapi;
     const sftp = await sftpClient({
       host: process.env.SFTP_HOST,
       port: Number(process.env.SFTP_PORT),
       username: process.env.SFTP_USER,
       password: process.env.SFTP_PASS,
     });
-    const uploadStream = sftp.createWriteStream(`${process.env.SFTP_UPLOAD_PATH}/${filename}`);
+    const uploadPath = path.join(process.env.SFTP_UPLOAD_PATH, path.basename(filename));
+    const uploadStream = sftp.createWriteStream(uploadPath) as SFTPWriteStream;
     file.pipe(uploadStream)
-      .on('close', () => reply({ filename }));
+      .on('close', (event: any) => {
+        const { bytesWritten } = uploadStream;
+        reply({ path: uploadPath, bytes: bytesWritten });
+      });
   } catch (err) {
     logger.error(err);
     return reply(Boom.badImplementation('There was a problem uploading the file'));
--- src/util/sftp.ts
+++ src/util/sftp.ts
@@ -1,5 +1,9 @@
 import { Client, ConnectConfig, SFTPWrapper } from 'ssh2';
+import { Writable } from 'stream';
 
+export interface SFTPWriteStream extends Writable {
+  bytesWritten: number;
+}
 export async function sftpClient(config: ConnectConfig) {
   return new Promise<SFTPWrapper>((resolve, reject) => {
     const client = new Client();
